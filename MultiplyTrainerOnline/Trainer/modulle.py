ab643975238b4a2f901d2936a97a6314d3830eb3
Tkachmaxim <tkachmaxim23@gmail.com>
2021-06-18 14:46:03 +0300

    creatingclass

--- MultiplyTrainerOnline/Trainer/modulle.py
+++ MultiplyTrainerOnline/Trainer/modulle.py
@@ -2,13 +2,13 @@ import random
 task=[]
 gen_task=[]
 
-def choice_number():
-    while True:
+def choice_number(n):
+    num=0
+    while num!=n:
         a = random.randrange(2, 10, 1)
         b = random.randrange(2, 10, 1)
-        gen_task = [a,b]
+        gen_task = dict(a=a, b=b, result=a*b)
         if gen_task not in task:
-            break
-    task.append(gen_task)
-    result=a*b
-    return a,b,result
+            num+=1
+            task.append(gen_task)
+    return task
