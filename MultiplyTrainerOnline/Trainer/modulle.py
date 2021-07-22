66a0f6c06887a1dcce6b0cbd449d10d2ad28e672
Tkachmaxim <tkachmaxim23@gmail.com>
2021-07-22 12:42:30 +0300

    add_divide_and_multyregime

--- MultiplyTrainerOnline/Trainer/modulle.py
+++ MultiplyTrainerOnline/Trainer/modulle.py
@@ -2,13 +2,45 @@ import random
 task=[]
 gen_task=[]
 
-def choice_number(n):
-    num=0
-    while num!=n:
-        a = random.randrange(2, 10, 1)
-        b = random.randrange(2, 10, 1)
-        gen_task = dict(a=a, b=b, result=a*b)
-        if gen_task not in task:
-            num+=1
-            task.append(gen_task)
-    return task
+def choice_number(n,action):
+    if action=='multi':
+        num=0
+        while num!=n:
+            gen_task=set_multy()
+            if  gen_task not in task:
+                num+=1
+                task.append(gen_task)
+        return task
+
+    if action=='divide':
+        num = 0
+        while num != n:
+           gen_task=set_divide()
+           if gen_task not in task:
+                num += 1
+                task.append(gen_task)
+        return task
+
+    if action=='complex':
+        num = 0
+        while num != n:
+            gen_task=random.choice([set_divide(), set_multy()])
+            print(gen_task)
+            if gen_task not in task:
+                num += 1
+                task.append(gen_task)
+        return task
+
+def set_multy():
+    a = random.randrange(2, 10, 1)
+    b = random.randrange(2, 10, 1)
+    task_ = str(a) + ' × ' + str(b)
+    gen_task = dict(a=task_, b=b, result=a * b)
+    return gen_task
+
+def set_divide():
+    a = random.randrange(2, 10, 1)
+    b = random.randrange(2, 10, 1)
+    task_ = str(a * b) + ':' + str(b)
+    gen_task = dict(a=task_, b=b, result=a)
+    return gen_task
