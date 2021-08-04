b7221e00ec7be20c4db7c3a7eef15e1bb091d165
Tkachmaxim <tkachmaxim23@gmail.com>
2021-08-04 09:40:12 +0300

    some_fixed

--- Trainer/admin.py
+++ Trainer/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Result
+from .models import Result, Game
 
 @admin.register(Result)
 class Result(admin.ModelAdmin):
     list_display = ('name', 'total_tasks', 'mistakes', 'examples_of_mistakes', 'time')
+
+@admin.register(Game)
+class Game(admin.ModelAdmin):
+    list_display = (
+        'permission', 'number_of_tasks', 'total', 'number_example',
+        'mistakes', 'mistakes_examples', 'tasks', 'total_for_save', 'mistakes_for_save', 'session', 'action'
+                    )
